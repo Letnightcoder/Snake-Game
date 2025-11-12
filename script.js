abfe8f229a25f402e647d977cd8b17a0233c880a
Official-Kallol <official.kallol2024@gmail.com>
2025-11-12 14:45:16 +0530

    Refactor HTML structure for improved readability; update JavaScript to manage high score, score, and time display in Snake Game

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ const startGamemodal = document.querySelector(".start-game");
 const gameOvermodal = document.querySelector(".game-over");
 const restartButton = document.querySelector(".btn-restart");
 
+const highScoreElement = document.querySelector("#high-score");
+const scoreElement = document.querySelector("#score");
+const timeElement = document.querySelector("#time");
+
 const blockHeight = 50;
 const blockWidth = 50;
 
+let highScore = Number(localStorage.getItem("highScore")) || 0;
+let score = 0
+let time = `00-00`
+
+highScoreElement.innerText = highScore;
+
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
 let intervalId = null;
+let timeIntervalId = null;
+
 let food = {
   x: Math.floor(Math.random() * rows),
   y: Math.floor(Math.random() * cols),
@@ -49,6 +62,7 @@ function render() {
     head = { x: snake[0].x - 1, y: snake[0].y };
   }
 
+  // wall collision logic
   if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
     clearInterval(intervalId);
 
@@ -59,6 +73,7 @@ function render() {
     return;
   }
 
+  // Food consume logic
   if (head.x == food.x && head.y == food.y) {
     blocks[`${food.x}-${food.y}`].classList.remove("food");
 
@@ -68,8 +83,15 @@ function render() {
     };
 
     blocks[`${food.x}-${food.y}`].classList.add("food");
-
     snake.unshift(head);
+
+    score += 10
+    scoreElement.innerText = score
+
+    if(score > highScore){
+      highScore = score
+      localStorage.setItem("highScore", highScore.toString())
+    }
   }
 
   snake.forEach((segment) => {
@@ -92,7 +114,21 @@ startButton.addEventListener("click", () => {
   modal.style.display = "none";
   intervalId = setInterval(() => {
     render();
-  }, 300);
+  }, 300)
+
+  timeIntervalId = setInterval(()=>{
+    let [min, sec] = time.split("-").map(Number)
+
+    if(sec == 59) {
+      min+=1
+      sec=0
+    }else{
+      sec+=1
+    }
+
+    time = `${min}-${sec}`
+    timeElement.innerText = time
+  }, 1000)
 });
 
 restartButton.addEventListener("click", restartGame);
@@ -103,6 +139,12 @@ function restartGame() {
   snake.forEach(segment => {
     blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
   })
+  score = 0
+  time = `00-00`
+
+  scoreElement.innerText = score
+  timeElement.innerText = time
+  highScoreElement.innerText = highScore
 
   modal.style.display = "none";
   direction = "down"
@@ -122,6 +164,8 @@ function restartGame() {
     render();
   }, 300);
 }
+
+
 
 addEventListener("keydown", (event) => {
   if (event.key == "ArrowUp") {
