7811d78ad51fd89ba4badcb8752b8b58c6c5bcce
Official-Kallol <official.kallol2024@gmail.com>
2025-11-12 12:18:53 +0530

    Add modal for game start and game over; implement start and restart functionality in Snake Game

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const board = document.querySelector(".board");
+const startButton = document.querySelector(".btn-start");
+const modal = document.querySelector(".modal");
+const startGamemodal = document.querySelector(".start-game");
+const gameOvermodal = document.querySelector(".game-over");
+const restartButton = document.querySelector(".btn-restart");
+
 const blockHeight = 50;
 const blockWidth = 50;
 
@@ -11,7 +17,7 @@ let food = {
 };
 
 const blocks = [];
-const snake = [
+let snake = [
   {
     x: 1,
     y: 3,
@@ -31,7 +37,7 @@ for (let row = 0; row < rows; row++) {
 function render() {
   let head = null;
 
-  blocks[ `${food.x}-${food.y}` ].classList.add("food")
+  blocks[`${food.x}-${food.y}`].classList.add("food");
 
   if (direction === "left") {
     head = { x: snake[0].x, y: snake[0].y - 1 };
@@ -44,11 +50,16 @@ function render() {
   }
 
   if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
-    alert("Game Over");
     clearInterval(intervalId);
+
+    modal.style.display = "flex";
+    startGamemodal.style.display = "none";
+    gameOvermodal.style.display = "flex";
+
+    return;
   }
 
-  if (head.x == food.x && head.y == food.y){
+  if (head.x == food.x && head.y == food.y) {
     blocks[`${food.x}-${food.y}`].classList.remove("food");
 
     food = {
@@ -58,7 +69,7 @@ function render() {
 
     blocks[`${food.x}-${food.y}`].classList.add("food");
 
-    snake.unshift(head)
+    snake.unshift(head);
   }
 
   snake.forEach((segment) => {
@@ -73,9 +84,44 @@ function render() {
   });
 }
 
-intervalId = setInterval(() => {
-  render();
-}, 300);
+// intervalId = setInterval(() => {
+//   render();
+// }, 300);
+
+startButton.addEventListener("click", () => {
+  modal.style.display = "none";
+  intervalId = setInterval(() => {
+    render();
+  }, 300);
+});
+
+restartButton.addEventListener("click", restartGame);
+
+function restartGame() {
+
+  blocks[`${food.x}-${food.y}`].classList.remove("food");
+  snake.forEach(segment => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+  })
+
+  modal.style.display = "none";
+  direction = "down"
+  snake = [
+    {
+      x: 1,
+      y: 3
+    },
+  ];
+
+  food = {
+    x: Math.floor(Math.random() * rows),
+    y: Math.floor(Math.random() * cols),
+  };
+
+  intervalId = setInterval(() => {
+    render();
+  }, 300);
+}
 
 addEventListener("keydown", (event) => {
   if (event.key == "ArrowUp") {
